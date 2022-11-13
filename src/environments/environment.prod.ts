cadd0b3340c449b02042c9e5dd706c10b133f78a
Roman Belik <rombelik@yandex.ru>
2022-11-13 21:23:49 +0300

    feat: add bff url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment: Config = {
     product: 'http://rombelik-cart-api-dev.eu-west-1.elasticbeanstalk.com',
     order: 'http://rombelik-cart-api-dev.eu-west-1.elasticbeanstalk.com',
     import: 'https://b8hevac9zk.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'http://rombelik-bff-api-dev.eu-west-1.elasticbeanstalk.com',
     cart: 'http://rombelik-cart-api-dev.eu-west-1.elasticbeanstalk.com'
   },
   apiEndpointsEnabled: {
